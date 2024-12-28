c725bcae4adec97c6c968257f6774133a4b88223
duccini <guilhermeduccini@gmail.com>
2024-12-28 16:54:48 -0300

    feat: add mint function to smart contract

--- contracts/NFTMarket.sol
+++ contracts/NFTMarket.sol
@@ -2,6 +2,84 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract NftMarket is ERC721URIStorage {
+  using Counters for Counters.Counter;
+
+  // If tokenId => NftItem, why NftItem need save tokenId??  
+  // STRUC CANNOT ENDED WITH `;`
+  struct NftItem {
+    uint tokenId;
+    uint price;
+    address creator;
+    bool isListed;
+  }
+
+  uint public listingPrice = 0.025 ether;
+
+  Counters.Counter private _listedItems;
+  Counters.Counter private _tokenIds;
+
+  mapping(string => bool) private _usedTokenURIs;
+  mapping(uint => NftItem) private _idToNftItem;
+
+  /*
+   *  Events you can emit from your functions and listener from your Front-End
+   *  
+   *  The last argument/propertie cannot ended with `,`
+   */
+  event NftItemCreated (
+    uint tokenId,
+    uint price,
+    address creator,
+    bool isListed
+  );
+
   constructor() ERC721("CreaturesNFT", "CNFT") {}
+
+  function getNftItem(uint tokenId) public view returns(NftItem memory) {
+    return _idToNftItem[tokenId];
+  }
+
+  function listedItemsCount() public view returns (uint) {
+    return _listedItems.current();
+  }
+
+  function tokenURIExists(string memory tokenURI) public view returns (bool) {
+    return _usedTokenURIs[tokenURI] == true;
+  }
+
+  /**
+   *  _safeMint defined in ERV721
+   *  _setTokenURI defined ERC721URIStorage
+   *  _function convenction for private function
+   */
+  function mintToken(string memory tokenURI, uint price) public payable returns (uint) {
+    // Prevent two token have same tokenURI
+    require(!tokenURIExists(tokenURI), "Token URI already exists.");
+    require(msg.value == listingPrice, "Price must be equal to listing price.");
+
+    _tokenIds.increment();
+    _listedItems.increment();
+
+    uint newTokenId = _tokenIds.current();
+
+    _safeMint(msg.sender, newTokenId);
+    _setTokenURI(newTokenId, tokenURI);
+
+    _usedTokenURIs[tokenURI] = true;
+
+    _createNftItem(newTokenId, price);
+
+    return newTokenId;
+  }
+
+  function _createNftItem(uint tokenId, uint price) private {
+    require(price > 0, "Price must be at least 1 wei.");
+
+    _idToNftItem[tokenId] = NftItem(tokenId, price, msg.sender, true);
+
+    emit NftItemCreated(tokenId, price, msg.sender, true);
+  }
 }
---
+++ test/nftMarket.test.js
@@ -0,0 +1,88 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+
+/**
+ *  This file will be executed by Truffle
+ *  Truffle will compile the Smart COntract and execute this test
+ *
+ *  truffle assertions npm
+ */
+const NftMarket = artifacts.require("NftMarket");
+const { ethers } = require("ethers");
+
+contract("NftMarket", (accounts) => {
+  let _contract = null;
+  let _nftPrice = ethers.utils.parseEther("0.25").toString();
+  let _listingPrice = ethers.utils.parseEther("0.025").toString();
+
+  before(async () => {
+    _contract = await NftMarket.deployed();
+  });
+
+  describe("Mint token", () => {
+    const tokenURI = "https://test.com";
+
+    /**
+     *  everytime you are calling a function of the a Smart Contract you can pass aditional
+     *  data, this additional data are passeded as an object, the last value of the function
+     *  you are calling
+     */
+    before(async () => {
+      await _contract.mintToken(tokenURI, _nftPrice, {
+        from: accounts[0],
+        value: _listingPrice,
+      });
+    });
+
+    it("owner of first token shoul be accounts[0]", async () => {
+      const owner = await _contract.ownerOf(1);
+
+      assert(
+        owner == accounts[0],
+        "Owner of first token is not matching accounts[0]"
+      );
+    });
+
+    it("first token should point to the correct tokenURI", async () => {
+      const actualTokenURI = await _contract.tokenURI(1);
+
+      assert.equal(
+        actualTokenURI,
+        "https://test.com",
+        "tokenURI is not correctly set."
+      );
+    });
+
+    it("should not be possible to create a NFT with used tokenURI", async () => {
+      try {
+        await _contract.mintToken(tokenURI, _nftPrice, {
+          from: accounts[0],
+          value: _listingPrice,
+        });
+      } catch (error) {
+        assert(error, "NFT was minted with previously used tokenURI");
+      }
+    });
+
+    it("should have one listed item", async () => {
+      const listedItemCount = await _contract.listedItemsCount();
+
+      // uint returns BigNumber
+      assert.equal(
+        listedItemCount.toNumber(),
+        "1",
+        "Listed items count is not 1."
+      );
+    });
+
+    it("should have have create NFT item", async () => {
+      const nftItem = await _contract.getNftItem(1);
+
+      // console.log(nftItem);
+
+      assert.equal(nftItem.tokenId, 1, "Token id is not 1");
+      assert.equal(nftItem.price, _nftPrice, "Nft price is not correct.");
+      assert.equal(nftItem.creator, accounts[0], "Creator is not account[0].");
+      assert.equal(nftItem.isListed, true, "Token is not listed.");
+    });
+  });
+});
